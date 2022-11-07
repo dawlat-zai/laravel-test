218b5ff0b82550853177b269f0d5d02cbb78e29c
dawlat <dawlat@pk.xtendum.com>
2022-11-07 22:43:13 +0500

    Task 1: get events with workshops

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -101,7 +101,7 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        return Event::with('workshops')->get();
     }
 
 
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,17 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
+    /**
+     * An event may have many workshops
+     *
+     * @return HasMany
+     */
+    public function workshops(): HasMany
+    {
+        return $this->hasMany(Workshop::class);
+    }
 }
